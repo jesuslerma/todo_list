4ce8db0d91e8e1974f40f859a10efcec9c187bac
jesus lerma sanchez <shuyojl@gmail.com>
2016-04-13 20:43:07 -0500

    pass test task without name must be invalid

--- test/models/task_test.rb
+++ test/models/task_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class TaskTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'task without name must be invalid' do 
+    task = Task.new title: nil
+    assert_not task.valid?
+  end
 end
